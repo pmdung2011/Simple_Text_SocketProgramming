9815c2b54d67d365e9c9e7dcdcf83f6d6648dd36
Danny Phung <54776410+pmdung2011@users.noreply.github.com>
2020-10-15 23:41:20 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,17 @@
 import socket
 import json
 
-s = socket.socket()
-host = socket.gethostname()
-port = 1234
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Initialize variables
+udp_port = 1234
+local_host = socket.gethostname()
+serverAddressPort = (local_host, udp_port)
+IPAddr = socket.gethostbyname(local_host)
+buffer_size = 1024
+
+UDPClientSocket.connect(serverAddressPort)
 
 
 class ProcessData:
@@ -11,10 +19,11 @@ class ProcessData:
     client_id = ""
     dest_id = ""
     send_text = ""
+    client_IP = ""
 
 
 def send_msg(sending_data):
-    s.send(sending_data.encode())
+    UDPClientSocket.sendto(sending_data.encode(), serverAddressPort)
 
 
 def request_options():
@@ -24,17 +33,15 @@ def request_options():
 
 
 variables = ProcessData()
-s.connect((host, port))
-data = s.recv(1024)
-print("Client received: ", data.decode('utf-8'))
+variables.client_IP = IPAddr
+
 print("Enter your ID as a string")
 variables.client_id = input("->")
-s.send(bytes(variables.client_id, "utf-8"))
+# UDPClientSocket.sendto(bytes(variables.client_id, "utf-8"), serverAddressPort)
+option = request_options()
 
-client_option = request_options()
-
-while client_option != "3":
-    while client_option == "1":
+while option != "3":
+    while option == "1":
         variables.client_code = "T"
         print("Enter dest ID as a string: ")
         variables.dest_id = input("-> ")
@@ -42,17 +49,17 @@ while client_option != "3":
         variables.send_text = input(request_send_text)
         send_data = json.dumps(
             {"code": variables.client_code, "dest_id": variables.dest_id, "client_id": variables.client_id,
-             "send_text": variables.send_text})
+             "send_text": variables.send_text, "client_ip": variables.client_IP})
         print("MESSAGE TO SEND: {} To: {} From: {} Text: {}".format(variables.client_code, variables.dest_id,
                                                                     variables.client_id, variables.send_text))
         send_msg(send_data)
         client_option = request_options()
 
-    while client_option == "2":
+    while option == "2":
         variables.client_code = "C"
         print("MESSAGE TO SEND: {} From: {} ".format(variables.client_code, variables.client_id))
         send_data = json.dumps({"code": variables.client_code, "client_id": variables.client_id})
         client_option = request_options()
 
-if client_option == "3":
-    s.close()
+if option == "3":
+    UDPClientSocket.close()
