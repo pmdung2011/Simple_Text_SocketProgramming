fc37ce008c83ccf971ab960d30fccbd97e725520
Danny Phung <54776410+pmdung2011@users.noreply.github.com>
2020-10-18 00:48:13 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,84 +1,80 @@
 import socket
 import json
+import threading
+import pickle
+from process_data import ProcessData
 
+# Create Socket options
 local_host = socket.gethostname()
 local_port = 1234
 UDPServerPort = (local_host, local_port)
 buffer_size = 1024
-# queue_size = 5
-msg_from_server = "Welcome to UDP server"
-bytes_to_send = str.encode(msg_from_server)
-
-# Create empty dictionary to store messages
-storage = {}
-
-
-class ProcessData:
-    client_code = ""
-    client_id = ""
-    dest_id = ""
-    send_text = ""
-    client_IP = ""
-
-    def __init__(self, data_object):
-        self.client_code = data_object["code"]
-        self.client_id = data_object["client_id"]
-        self.dest_id = data_object["dest_id"]
-        self.send_text = data_object["send_text"]
-        self.client_IP = data_object["client_ip"]
-
-
-def check_code(received_data, c_code):
-    if c_code == "T":
-        storage[received_data.dest_id] = received_data.send_text
-    else:
-        if received_data.dest_id in storage.keys():
-            result = storage[received_data.client_IP]
-            storage.pop(received_data.dest_id)
-            return result
-        else:
-            result = "No Text"
-            return result
-
+socket_lock = threading.Lock()
 
 # Create a UDP socket
 UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 # Bind to port
 UDPServerSocket.bind(UDPServerPort)
 welcome_message = "SERVER IS NOW LISTENING AT UDP SOCKET"
 print(welcome_message)
 
+# Create empty dictionary to store messages
+storage = {}
 
-while True:
-    data, client_address = UDPServerSocket.recvfrom(buffer_size*2)
-    print("MESSAGE RECEIVED: ", data.decode('utf-8'))
-    print('The client at {} says {!r}'.format(client_address, data))
 
+def handle_socket(data, client_address, local_storage):
+    # print('The client at {} says {!r}'.format(client_address, data))
     # Load received data into JSON
     data_receive = json.loads(data.decode())
 
     # Split JSON data into specific parts
-    processData = ProcessData(data_receive)
+    process_data = ProcessData(data_receive)
 
-    # Specify client_ip
-    processData.client_IP = client_address
-
-    # Specify client_name
-    client_name = processData.client_id
-
-    text_msg = processData.send_text
-    dest_name = processData.dest_id
     # Verify if the client_code is valid
-    client_code = processData.client_code
-    if processData.client_code != "T" or processData.client_code != "C":
-        warning_msg = "Invalid <code> format. "
-        UDPServerSocket.sendto(bytes(warning_msg, "utf-8"), client_address)
+    client_code = process_data.client_code
+
+    # Verify client code
+    if client_code == "T":
+
+        print("MESSAGE RECEIVED: T To: {} From: {} Text: {}".format(process_data.dest_id, process_data.client_id,
+                                                                    process_data.send_text))
+        if process_data.dest_id not in local_storage:
+            local_storage[process_data.dest_id] = []  # Create a queue to store data with the same key
+        # local_storage[process_data.dest_id].append(process_data.send_text)
+        local_storage[process_data.dest_id].append(process_data)
+        # print("Storage: ")
+        # print(local_storage)
+        result = json.dumps({"message": "messages stored"})
+
+    elif client_code == "C":
+        if process_data.client_id in local_storage.keys():
+            # text = "\n".join(local_storage[process_data.client_id])
+            # del local_storage[process_data.client_id]
+            obj = local_storage[process_data.client_id].pop(0)  # Pop out relative object
+            if obj.send_text is not None:
+                result = json.dumps({"message": obj.send_text, "sender": obj.client_id})
+                print("MESSAGE TO SEND: T To: {} From: {} Text: {}".format(obj.dest_id, obj.client_id, result))
+            else:
+                result = json.dumps({"message": "No Text", "sender": obj.client_id})
+                print("MESSAGE TO SEND: T To: {} Text: No Text".format(obj.client_id))
+        else:
+            result = json.dumps({"message": "No Text"})
+            print("MESSAGE TO SEND: T To: {} Text: No Text".format(process_data.client_id))
+
     else:
-        feedback_data = check_code(processData, client_code)
-        print("Storage: ")
-        print(storage)
-        UDPServerSocket.sendto(feedback_data, client_address)
+        result = json.dumps({"message": "Invalid <code> format. "})
+    # print(result)
+    # result = str.encode(result)
+    with socket_lock:
+        UDPServerSocket.sendto(result.encode(), client_address)
 
 
-
-
+while True:
+    processing_data, processing_client_address = UDPServerSocket.recvfrom(buffer_size * 2)
+    # print("receiving data from ")
+    # print(processing_client_address)
+    # Create threads to handle multiple clients
+    thread = threading.Thread(target=handle_socket, args=(processing_data, processing_client_address, storage))
+    thread.daemon = True
+    thread.start()
